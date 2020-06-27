8c8f680d07f5c95572deab6277d327accbb19b3c
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-06-27 00:53:25 -0400

    clean up

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,9 @@
 import { APIGatewayEvent, Context, Callback } from "aws-lambda";
 import EventController from './controllers/eventController';
 
-/** Acts as a router for the event http methods -- directing each request to the appropriate controller function */
+/** Acts as a router for the event http methods -- directing each request to the appropriate controller function 
+ * Normally, this would be an Express Router rather than this custom switcher
+*/
 const ApiRouter = (event: APIGatewayEvent, context: Context, callback: Callback) => {
 
     switch (event.httpMethod) {
