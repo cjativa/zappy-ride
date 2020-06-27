7b56fb6f4c835b85505b92e113a7c1d86357161e
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-06-27 00:53:02 -0400

    removed default callback handler

--- handler.ts
+++ handler.ts
@@ -7,14 +7,4 @@ export const handler = (event: APIGatewayEvent, context: Context, callback: Call
     console.log(`You hit /events`);
 
     ApiRouter(event, context, callback);
-
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify({
-            message: 'Go Serverless v1.0! Your function executed successfully!',
-            input: event,
-        }),
-    };
-
-    callback(null, response);
 }
