c26553e461c9f00c06fe6450aa009081cfe8482d
Christopher D Jativa <cjativa@barnesfoundation.org>
2020-06-27 00:53:15 -0400

    added database name to config

--- api/config.ts
+++ api/config.ts
@@ -1,8 +1,8 @@
-import dotenv from 'dotenv';
 
 const Config = {
 
     databaseHost: process.env.DATABASE_HOST!,
+    databaseName: process.env.DATABASE_NAME!,
     databaseUser: process.env.DATABASE_USER!,
     databasePassword: process.env.DATABASE_PASSWORD!,
     databasePort: parseInt(process.env.DATABASE_PORT!),    
